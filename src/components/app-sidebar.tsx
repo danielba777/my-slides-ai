0084797165a6e481150c272cefa89aa31c51d7b5
Daniel <danielbauer230501@gmail.com>
2025-10-15 15:56:33 +0200

    Added AccountButton in Sidebar

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -3,6 +3,7 @@ import { IoHomeOutline, IoImagesOutline } from "react-icons/io5";
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
@@ -10,14 +11,18 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { SidebarAccountSection } from "./app-sidebar-account";
 
-// Menu items.
-const items = [
+const startItems = [
   {
     title: "Home",
     url: "/dashboard/home",
     icon: IoHomeOutline,
   },
+];
+
+// Menu items.
+const playgroundItems = [
   {
     title: "Slideshows",
     url: "/dashboard/slideshows",
@@ -30,10 +35,26 @@ export function AppSidebar() {
     <Sidebar>
       <SidebarContent>
         <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {startItems.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+        <SidebarGroup>
           <SidebarGroupLabel>Playground</SidebarGroupLabel>
           <SidebarGroupContent>
             <SidebarMenu>
-              {items.map((item) => (
+              {playgroundItems.map((item) => (
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton asChild>
                     <a href={item.url}>
@@ -47,6 +68,9 @@ export function AppSidebar() {
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
+      <SidebarFooter>
+        <SidebarAccountSection />
+      </SidebarFooter>
     </Sidebar>
   );
 }
